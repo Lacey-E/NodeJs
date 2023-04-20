31329d00c320e978cd013301c75e921de9890759
Lacey-E <stevealila25@gmail.com>
2023-04-20 08:29:08 -0600

    mmm

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(3000, "localhost", () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log("Listening for request on port 3000");
 });
